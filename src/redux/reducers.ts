bae3d2cf004c8e5387bcca8cd4fa884e47c8b70d
Ivan Kosolapov <kosolapow.iwan@gmail.com>
2017-08-31 15:18:29 +0300

    Simplify reducers.

--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,16 +1,28 @@
 import { combineReducers } from 'redux';
 import { Map } from 'immutable';
 
-import { State, Message, MessageId, Votes } from './state';
+import { State, Message, MessageId, Votes, Messages } from './state';
 import { Action, VOTE_BUG, VOTE_NOT_BUG } from './actions';
+import { isNullOrUndefined } from 'util';
+
+const getNextMessage = (messages: Map<MessageId, Message>): Message | undefined =>
+  messages.find((message: Message) => isNullOrUndefined(message.vote));
 
 const rootReducer = combineReducers<State>({
-  messages: (state: Map<MessageId, Message> = Map<MessageId, Message>(), action: Action): Map<MessageId, Message> => {
+  messages: (state: Messages = {all: Map<MessageId, Message>()},
+             action: Action): Messages => {
+
     switch (action.type) {
       case VOTE_BUG:
-        return state.setIn([0, 'vote'], Votes.BUG);
+        return {
+          currentMessage: getNextMessage(state.all),
+          all: state.all.setIn([0, 'vote'], Votes.BUG)
+        };
       case VOTE_NOT_BUG:
-        return state.setIn([0, 'vote'], Votes.NOT_BUG);
+        return {
+          currentMessage: getNextMessage(state.all),
+          all: state.all.setIn([0, 'vote'], Votes.NOT_BUG)
+        };
       default:
         return state;
     }
--- src/redux/state.ts
+++ src/redux/state.ts
@@ -8,28 +8,35 @@ export enum Votes {
 export type MessageId = number;
 
 export type Message = {
-  readonly id: MessageId;
-  readonly text: string;
+  readonly id: MessageId
+  readonly text: string
   readonly vote?: Votes
 };
 
+export type Messages = {
+  readonly currentMessage?: Message
+  readonly all: Map<MessageId, Message>
+};
+
 export type State = {
-  readonly messages: Map<MessageId, Message>
+  readonly messages: Messages
 };
 
 export const initialState: State = {
-  messages: Map<MessageId, Message>({
-    0: {
-      id: 0,
-      text: 'I acquire this ionic cannon, it\'s called reliable flight.',
-    },
-    1: {
-      id: 1,
-      text: 'Engage, strange energy!',
-    },
-    2: {
-      id: 2,
-      text: 'All the ships deceive bare, spheroid cosmonauts.',
-    },
-  })
+  messages: {
+    all: Map<MessageId, Message>({
+      0: {
+        id: 0,
+        text: 'I acquire this ionic cannon, it\'s called reliable flight.',
+      },
+      1: {
+        id: 1,
+        text: 'Engage, strange energy!',
+      },
+      2: {
+        id: 2,
+        text: 'All the ships deceive bare, spheroid cosmonauts.',
+      },
+    })
+  }
 };
